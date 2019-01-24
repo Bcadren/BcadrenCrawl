9dbd207e5427c1bace73e3634a0b45499e129ab7
Edgar A. Bering IV <trizor@gmail.com>
2019-01-23 19:08:31 -0500

    Scale net trap to-hit with absdepth
    Legacy net traps had a fixed to-hit chance; with the change to make them
    always player targeted it is readily apparent that their ability to hit
    falls off in the late game, making them more a free source of nets than
    a relevant tactical threat. They now scale with approximately the
    throwing abilities of a non-archer see-invisible monster with HD absdepth/3.

--- crawl-ref/source/traps.cc
+++ crawl-ref/source/traps.cc
@@ -658,8 +658,8 @@ void trap_def::trigger(actor& triggerer)
 					item_def item = generate_trap_item();
 					copy_item_to_grid(item, triggerer.pos());
 
-					if (random2limit(you.evasion(), 40)
-						+ random2(4) + 3 > 12)
+					if (random2avg(2 * you.evasion(), 2)
+				        > 18 + env.absdepth0 / 2)
 					{
 						mpr("A net drops to the ground!");
 					}
