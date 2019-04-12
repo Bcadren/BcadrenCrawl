bfb02eb66168ed39d1fe2fd8582179255ffddb35
advil <rawlins@gmail.com>
2019-04-12 12:17:48 -0400

    Prevent wand of enslavement from being used under sac love
    Before this commit, despite being grayed out, this wand worked just
    fine. The implementation, in order not to waste turns, just makes it
    completely unevokable with sac love. For the sake of consistency I
    applied this change to all evokers that are useless in this case. An
    alternative for enslavement might be to prevent it in targeting, which
    is how phantom mirror previously worked, but the present approach seemed
    cleaner and wastes the players time less. It might also be good to make
    a note in the item descriptions that they can't be used so this isn't
    indicated only by inventory color.
    
    In general, enslavement still works under other god conducts that
    otherwise prevent some but not all summons. This probably could use some
    thinking, because as NP7 pointed out in chat, it leads to confusing and
    variable behavior e.g. for spellcasters enslaved under Trog. However, to
    implement this without taking turns in the general case is a bit tricky.
    See also 706f6395352a.

--- crawl-ref/source/invent.cc
+++ crawl-ref/source/invent.cc
@@ -2005,6 +2005,27 @@ bool item_is_wieldable(const item_def &item)
     return (is_weapon(item) || item.base_type == OBJ_SHIELDS) && you.species != SP_FELID;
 }
 
+/// Does the item only serve to produce summons or allies?
+static bool _item_ally_only(const item_def &item)
+{
+    if (item.base_type == OBJ_WANDS)
+        return item.sub_type == WAND_ENSLAVEMENT;
+    else if (item.base_type == OBJ_MISCELLANY)
+    {
+        switch (item.sub_type)
+        {
+        case MISC_PHANTOM_MIRROR:
+        case MISC_HORN_OF_GERYON:
+        case MISC_BOX_OF_BEASTS:
+        case MISC_SACK_OF_SPIDERS:
+            return true;
+        default:
+            return false;
+        }
+    }
+    return false;
+}
+
 /**
  * Return whether an item can be evoked.
  *
@@ -2065,6 +2086,14 @@ bool item_is_evokable(const item_def &item, bool reach, bool known,
                                    && !item_is_melded(item) ||
 								   you.equip[EQ_WEAPON1] == item.link
 								   && !item_is_melded(item);
+
+    // TODO: check other summoning constraints here?
+    if (_item_ally_only(item) && you.has_mutation(MUT_NO_LOVE))
+    {
+        if (msg)
+            mpr("That item cannot be used by those hated by all!");
+        return false;
+    }
 
     switch (item.base_type)
     {
