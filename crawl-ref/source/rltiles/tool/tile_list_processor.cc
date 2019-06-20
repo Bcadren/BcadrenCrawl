6cd24b6d1b6eb31a0c3aa80459cc54c70aaf8923
advil <rawlins@gmail.com>
2019-06-20 11:39:39 -0400

    Don't let locale-specific casing affect ascii characters
    If a locale changes the upper/lower case correspondents of an ascii
    character, this would break many things. For example, as in #846, in
    Turkish (tr_TR.utf8), the lowercase version of I is a dotless i (ı).
    Since these case functions are used for all sorts of internal purposes
    (e.g. producing filenames from capitalized tile names), crawl would
    fail to even start. Though this issue only illustrated the first error,
    this is the tip of the iceberg and nearly every use of lowercasing was
    broken in this locale.
    
    I think, ideally, we'd be using two different case swapping functions:
    one for internal data (which is always ASCII) and one for UI-facing
    strings (which may be in arbitrary UTF-8 encodings). However, when I
    tried to adjust the API along these lines to fix Turkish, it turned out
    that there is not a clean distinction in crawlcode between these two
    uses and even stuff like the name functions which might be expected to
    be UI-facing are used for internal purposes as well. Based on my
    attempts, if this does come up for what little localization we have, it
    would be better/safer to let the ASCII behavior be the default for ascii
    characters and add in localized casing piecemeal.
    
    This commit also adjusts various hotkeys, and I'm not sure what impact
    this will have (it might be going too far); it could cause problems to
    e.g. users on a Turkish keyboard.

--- crawl-ref/source/rltiles/tool/tile_list_processor.cc
+++ crawl-ref/source/rltiles/tool/tile_list_processor.cc
@@ -190,7 +190,7 @@ static int str_to_colour(string colour)
         return 0;
 
     for (unsigned int c = 0; c < colour.size(); c++)
-        colour[c] = tolower(colour[c]);
+        colour[c] = toalower(colour[c]);
 
     for (int i = 0; i < 16; ++i)
     {
@@ -894,8 +894,8 @@ bool tile_list_processor::write_data(bool image, bool code)
     string ucname = m_name;
     for (unsigned int i = 0; i < m_name.size(); i++)
     {
-        lcname[i] = tolower(m_name[i]);
-        ucname[i] = toupper(m_name[i]);
+        lcname[i] = toalower(m_name[i]);
+        ucname[i] = toaupper(m_name[i]);
     }
     string max = m_prefix;
     max += "_";
@@ -1061,7 +1061,7 @@ bool tile_list_processor::write_data(bool image, bool code)
             max_enum += "_MAX";
 
             for (char& c : max_enum)
-                c = toupper(c);
+                c = toaupper(c);
 
             fprintf(fp, "    %s_%s_MAX = %s\n};\n\n", m_prefix.c_str(), ucname.c_str(), max_enum.c_str());
         }
@@ -1118,6 +1118,7 @@ bool tile_list_processor::write_data(bool image, bool code)
 
         fprintf(fp, "// This file has been automatically generated.\n\n");
         fprintf(fp, "#include \"AppHdr.h\"\n");
+        fprintf(fp, "#include \"libutil.h\"\n");
         fprintf(fp, "#include \"tiledef-%s.h\"\n\n", lcname.c_str());
 
         fprintf(fp, "static unsigned int _tile_%s_count[%s - %s] =\n{\n",
@@ -1264,7 +1265,7 @@ bool tile_list_processor::write_data(bool image, bool code)
 
                 string lcenum = enumname;
                 for (unsigned int c = 0; c < enumname.size(); c++)
-                    lcenum[c] = tolower(enumname[c]);
+                    lcenum[c] = toalower(enumname[c]);
 
                 table.insert(sort_map::value_type(lcenum, i));
             }
@@ -1288,7 +1289,7 @@ bool tile_list_processor::write_data(bool image, bool code)
             "\n"
             "    string lc = str;\n"
             "    for (unsigned int i = 0; i < lc.size(); i++)\n"
-            "        lc[i] = tolower(lc[i]);\n"
+            "        lc[i] = toalower(lc[i]);\n"
             "\n"
             "    int num_pairs = sizeof(%s_name_pairs) / sizeof(%s_name_pairs[0]);\n"
             "    bool result = binary_search<const char *, tileidx_t>(\n"
@@ -1377,7 +1378,7 @@ bool tile_list_processor::write_data(bool image, bool code)
             max_enum += "_MAX";
 
             for (char& c : max_enum)
-                c = toupper(c);
+                c = toaupper(c);
 
             uc_max_enum.push_back(max_enum);
 
@@ -1478,7 +1479,7 @@ bool tile_list_processor::write_data(bool image, bool code)
             {
                 string lcenum = m_page.m_tiles[i]->enumname(0);
                 for (unsigned int c = 0; c < lcenum.size(); c++)
-                    lcenum[c] = tolower(lcenum[c]);
+                    lcenum[c] = toalower(lcenum[c]);
 
                 if (i == 0 || m_page.m_counts[i] == 1)
                     fprintf(fp, "<td>%s</td>", lcenum.c_str());
@@ -1710,7 +1711,7 @@ bool tile_list_processor::write_data(bool image, bool code)
                 max_enum += "_MAX";
 
                 for (char& c : max_enum)
-                    c = toupper(c);
+                    c = toaupper(c);
 
                 fprintf(fp, "exports.%s_MAX = window.%s_%s_MAX = %s.%s;\n\n",
                         ucname.c_str(), m_prefix.c_str(), ucname.c_str()
@@ -1725,7 +1726,7 @@ bool tile_list_processor::write_data(bool image, bool code)
                 string max_enum = abstract.first;
                 max_enum += "_MAX";
                 for (char& c : max_enum)
-                    c = toupper(c);
+                    c = toaupper(c);
 
                 max_enum = abstract.first + "." + max_enum;
 
