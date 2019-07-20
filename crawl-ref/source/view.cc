2d4ed2354b60d517ebaf2f3254146ed6e817e557
advil <rawlins@gmail.com>
2019-07-20 15:17:44 -0400

    Revert "Don't crash if redrawing a prompt triggers a flash"
    This reverts commit a379d5e2002d1753cc2fdd47a06dc70336d03bd7.

--- crawl-ref/source/view.cc
+++ crawl-ref/source/view.cc
@@ -959,10 +959,7 @@ void flash_view(use_animation_type a, colour_t colour, targeter *where)
     {
         you.flash_colour = colour;
         you.flash_where = where;
-        const auto region = get_cursor_region();
-        const coord_def pos = cgetpos(region);
         viewwindow(false);
-        cgotoxy(pos.x, pos.y, region);
     }
 }
 
@@ -1489,7 +1486,6 @@ void viewwindow(bool show_updates, bool tiles_only, animation *a)
         {
             puttext(crawl_view.viewp.x, crawl_view.viewp.y, crawl_view.vbuf);
             update_monster_pane();
-            // these can change region...
         }
 #endif
 #ifdef USE_TILE
