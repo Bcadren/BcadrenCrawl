d8dce053e3893f09f8c95bc376969ad94bba661e
Lucien <bhylton7@gmail.com>
2019-11-18 13:32:37 -0800

    Use normal GDR instead of 2x GDR for AC_TRIPLE (LRD damage).

--- crawl-ref/source/actor.cc
+++ crawl-ref/source/actor.cc
@@ -371,9 +371,6 @@ int actor::apply_ac(int damage, int max_damage, ac_type ac_rule,
     case AC_TRIPLE:
         saved = random2(1 + ac) + random2(1 + ac) + random2(1 + ac);
         ac *= 3;
-        // apply GDR only twice rather than thrice, that's probably still waaay
-        // too good. 50% gives 75% rather than 100%, too.
-        gdr = 100 - gdr * gdr / 100;
         break;
     default:
         die("invalid AC rule");
