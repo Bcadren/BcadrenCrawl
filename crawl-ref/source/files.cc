9c55ba270ac2d491d832246d2d3afb840451bd45
Lucien <bhylton7@gmail.com>
2019-11-18 20:33:54 -0800

    Add colour.h to the dependencies of files.cc

--- crawl-ref/source/files.cc
+++ crawl-ref/source/files.cc
@@ -32,6 +32,7 @@
 #include "butcher.h" // for fedhas_rot_all_corpses
 #include "chardump.h"
 #include "cloud.h"
+#include "colour.h"
 #include "coordit.h"
 #include "dactions.h"
 #include "dgn-overview.h"
