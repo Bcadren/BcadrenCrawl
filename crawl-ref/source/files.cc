crawl/crawl

Place AK start in abyss with pregeneration on (11871)
The chapter check was basically in the wrong place; moved it up to
outside the builder call. Also, just in case, unwind the chapter
variable while doing pregeneration so that nothing can condition on it
incorrectly.

crawl-ref/source/files.cc
@@ -1248,15 +1248,6 @@ static void _make_level(dungeon_feature_type stair_taken,
 
     env.turns_on_level = -1;
 
-    if (you.chapter == CHAPTER_POCKET_ABYSS
-        && player_in_branch(BRANCH_DUNGEON))
-    {
-        // If we're leaving the Abyss for the first time as a Chaos
-        // Knight of Lugonu (who start out there), enable normal monster
-        // generation.
-        you.chapter = CHAPTER_ORB_HUNTING;
-    }
-
     tile_init_default_flavour();
     tile_clear_flavour();
     env.tile_names.clear();
@@ -1544,6 +1535,16 @@ bool load_level(dungeon_feature_type stair_taken, load_mode_type load_mode,
 #endif
 
     bool just_created_level = false;
+
+    if (load_mode != LOAD_GENERATE
+        && you.chapter == CHAPTER_POCKET_ABYSS
+        && player_in_branch(BRANCH_DUNGEON))
+    {
+        // If we're leaving the Abyss for the first time as a Chaos
+        // Knight of Lugonu (who start out there), enable normal monster
+        // generation.
+        you.chapter = CHAPTER_ORB_HUNTING;
+    }
 
     // GENERATE new level when the file can't be opened:
     if (!you.save->has_chunk(level_name))

Bcadren/BcadrenCrawl

crawl-ref/source/files.cc
@@ -1250,15 +1250,6 @@ static void _make_level(dungeon_feature_type stair_taken,
 
     env.turns_on_level = -1;
 
-    if (you.chapter == CHAPTER_NONDUNGEON_START
-        && player_in_branch(BRANCH_DUNGEON))
-    {
-        // Preparations for entering the dungeon for the first time.
-		delete_level(level_id(BRANCH_START_MARKET, 1));
-		delete_level(level_id(BRANCH_START_TEMPLE, 1));
-        you.chapter = CHAPTER_ORB_HUNTING;
-    }
-
     tile_init_default_flavour();
     tile_clear_flavour();
     env.tile_names.clear();
@@ -1560,6 +1551,16 @@ bool load_level(dungeon_feature_type stair_taken, load_mode_type load_mode,
 #endif
 
     bool just_created_level = false;
+
+    if (load_mode != LOAD_GENERATE
+        && you.chapter == CHAPTER_NONDUNGEON_START
+        && player_in_branch(BRANCH_DUNGEON))
+    {
+		// Preparations for entering the dungeon for the first time.
+		delete_level(level_id(BRANCH_START_MARKET, 1));
+		delete_level(level_id(BRANCH_START_TEMPLE, 1));
+        you.chapter = CHAPTER_ORB_HUNTING;
+    }
 
     // GENERATE new level when the file can't be opened:
     if (!you.save->has_chunk(level_name))