Hellmonk/bloatcrawl2

Implement dungeon pregeneration (bel)
In order to improve deterministic dungeons (given a seed), allow
pregenerating the entire dungeon in a stable order. This is originally
based on an implementation by bel, that can be found at:
    https://github.com/belrogue/crawl/compare/master...belrogue:seeded_crawl

That approach relied on the player actually entering each level, which
had a number of weird side-effects, and relied on wizmode code (not
available in all builds). This commit attempts to separate out
generation and entering of a level, where a non-visited level will have
0 turns spent on it. It also simplifies the generation order from bel's
version. To enable this, set the option `pregen_dungeon` to true.
Pregeneration is disabled for DGL builds, for two reasons: I'm assuming
seeded runs won't be a thing any time soon for server play, and
pregeneration is a heavy initial CPU hit that might impact server
performance in any case.

There are still a bunch of UI issues after this commit, and it's not
super well tested.

Other side effects:
 * Added an initialization phase for map markers that can run when a
   level is generated - activation happens when a player enters the
   level.

crawl-ref/source/startup.cc
@@ -16,6 +16,7 @@
 #include "database.h"
 #include "dbg-maps.h"
 #include "dbg-objstat.h"
+#include "dgn-overview.h"
 #include "dungeon.h"
 #include "end.h"
 #include "exclude.h"
@@ -43,6 +44,7 @@
 #include "ng-setup.h"
 #include "notes.h"
 #include "output.h"
+#include "place.h"
 #include "player-save-info.h"
 #include "shopping.h"
 #include "skills.h"
@@ -235,6 +237,114 @@ static void _zap_los_monsters(bool items_also)
     }
 }
 
+#ifndef DGAMELAUNCH
+/**
+ * Ensure that the level given by `pos` is generated. This does not do much in
+ * the way of cleanup, and the caller must ensure the player ends up somewhere
+ * sensible (this will not place the player).
+ */
+static bool _ensure_level_generated(const level_pos &pos)
+{
+    // TODO: how important is it to get stair_taken right? bel just used stone 1
+    dungeon_feature_type stair_taken =
+        absdungeon_depth(pos.id.branch, pos.id.depth) > env.absdepth0 ?
+        DNGN_STONE_STAIRS_DOWN_I : DNGN_STONE_STAIRS_UP_I;
+
+    if (pos.id.depth == brdepth[pos.id.branch])
+        stair_taken = DNGN_STONE_STAIRS_DOWN_I;
+
+    if (!player_in_branch(pos.id.branch) && pos.id.depth == 1
+        && pos.id.branch != BRANCH_DUNGEON)
+    {
+        stair_taken = branches[pos.id.branch].entry_stairs;
+    }
+
+    if (is_connected_branch(pos.id.branch))
+        you.level_stack.clear();
+    else
+    {
+        for (int i = you.level_stack.size() - 1; i >= 0; i--)
+            if (you.level_stack[i].id == pos.id)
+                you.level_stack.resize(i);
+        if (!player_in_branch(pos.id.branch))
+            you.level_stack.push_back(level_pos::current());
+    }
+
+    const level_id old_level = level_id::current();
+
+    you.where_are_you = static_cast<branch_type>(pos.id.branch);
+    you.depth         = pos.id.depth;
+
+    leaving_level_now(stair_taken);
+    return load_level(stair_taken, LOAD_GENERATE, old_level);
+}
+
+static void _pregen_levels(const branch_type branch)
+{
+    unwind_bool no_more(crawl_state.show_more_prompt, false);
+
+    for (int i = 1; i <= branches[branch].numlevels; i++)
+    {
+        level_id new_level = level_id(branch, i);
+        level_pos pos = level_pos(new_level);
+        dprf("Pregenerating %s:%d", branches[pos.id.branch].abbrevname,
+                                    pos.id.depth);
+        _ensure_level_generated(pos);
+    }
+}
+
+static void _pregen_dungeon()
+{
+    mpr("Press <space> to pregenerate (most of) the dungeon."
+            " This will take some time.");
+    more();
+
+    const level_id &pregen_old_level = level_id::current();
+    const coord_def &pregen_old_pos = you.pos();
+    level_pos old_level_pos = level_pos(pregen_old_level, pregen_old_pos);
+    dprf("Before pregen; branch: %d depth: %d Coord: %d, %d.",
+            old_level_pos.id.branch, old_level_pos.id.depth,
+            old_level_pos.pos.x, old_level_pos.pos.y);
+
+    // bel's original proposal generated D to lair depth, then lair, then D
+    // to orc depth, then orc, then the rest of D. I have simplified this to
+    // just generate whole branches at a time -- I am not sure how much real
+    // impact this has. One idea might be to shuffle this slightly based on
+    // the seed.
+    // TODO: probably need to do portal vaults too?
+    // Should this use something like logical_branch_order?
+    const vector<branch_type> generation_order =
+    {
+        BRANCH_DUNGEON,
+        BRANCH_TEMPLE,
+        BRANCH_LAIR,
+        BRANCH_ORC,
+        BRANCH_SPIDER,
+        BRANCH_SNAKE,
+        BRANCH_SHOALS,
+        BRANCH_SWAMP,
+        BRANCH_VAULTS,
+        BRANCH_CRYPT,
+        BRANCH_DEPTHS,
+        BRANCH_VESTIBULE,
+        BRANCH_ELF,
+        BRANCH_ZOT,
+        BRANCH_SLIME,
+        BRANCH_TOMB,
+        BRANCH_TARTARUS,
+        BRANCH_COCYTUS,
+        BRANCH_DIS,
+        BRANCH_GEHENNA,
+    };
+
+    // TODO: why is dungeon invalid? it's not set up properly in
+    // `initialise_branch_depths` for some reason.
+    for (auto br : generation_order)
+        if (brentry[br].is_valid() || br == BRANCH_DUNGEON)
+            _pregen_levels(br);
+}
+#endif
+
 static void _post_init(bool newc)
 {
     ASSERT(strwidth(you.your_name) <= MAX_NAME_LENGTH);
@@ -258,11 +368,22 @@ static void _post_init(bool newc)
 
     run_map_local_preludes();
 
-    // Abyssal Knights start out in the Abyss.
-    if (newc && you.chapter == CHAPTER_POCKET_ABYSS)
-        you.where_are_you = BRANCH_ABYSS;
-    else if (newc)
-        you.where_are_you = root_branch;
+    if (newc)
+    {
+#ifndef DGAMELAUNCH
+        if (Options.pregen_dungeon)
+            _pregen_dungeon();
+#endif
+
+        you.entering_level = false;
+        you.transit_stair = DNGN_UNSEEN;
+        you.depth = 1;
+        // Abyssal Knights start out in the Abyss.
+        if (you.chapter == CHAPTER_POCKET_ABYSS)
+            you.where_are_you = BRANCH_ABYSS;
+        else
+            you.where_are_you = root_branch;
+    }
 
     // XXX: Any invalid level_id should do.
     level_id old_level;

Bcadren/BcadrenCrawl

crawl-ref/source/startup.cc
@@ -16,6 +16,7 @@
 #include "database.h"
 #include "dbg-maps.h"
 #include "dbg-objstat.h"
+#include "dgn-overview.h"
 #include "dungeon.h"
 #include "end.h"
 #include "exclude.h"
@@ -43,6 +44,7 @@
 #include "ng-setup.h"
 #include "notes.h"
 #include "output.h"
+#include "place.h"
 #include "player-save-info.h"
 #include "shopping.h"
 #include "skills.h"
@@ -235,6 +237,114 @@ static void _zap_los_monsters(bool items_also)
     }
 }
 
+#ifndef DGAMELAUNCH
+/**
+ * Ensure that the level given by `pos` is generated. This does not do much in
+ * the way of cleanup, and the caller must ensure the player ends up somewhere
+ * sensible (this will not place the player).
+ */
+static bool _ensure_level_generated(const level_pos &pos)
+{
+    // TODO: how important is it to get stair_taken right? bel just used stone 1
+    dungeon_feature_type stair_taken =
+        absdungeon_depth(pos.id.branch, pos.id.depth) > env.absdepth0 ?
+        DNGN_STONE_STAIRS_DOWN_I : DNGN_STONE_STAIRS_UP_I;
+
+    if (pos.id.depth == brdepth[pos.id.branch])
+        stair_taken = DNGN_STONE_STAIRS_DOWN_I;
+
+    if (!player_in_branch(pos.id.branch) && pos.id.depth == 1
+        && pos.id.branch != BRANCH_DUNGEON)
+    {
+        stair_taken = branches[pos.id.branch].entry_stairs;
+    }
+
+    if (is_connected_branch(pos.id.branch))
+        you.level_stack.clear();
+    else
+    {
+        for (int i = you.level_stack.size() - 1; i >= 0; i--)
+            if (you.level_stack[i].id == pos.id)
+                you.level_stack.resize(i);
+        if (!player_in_branch(pos.id.branch))
+            you.level_stack.push_back(level_pos::current());
+    }
+
+    const level_id old_level = level_id::current();
+
+    you.where_are_you = static_cast<branch_type>(pos.id.branch);
+    you.depth         = pos.id.depth;
+
+    leaving_level_now(stair_taken);
+    return load_level(stair_taken, LOAD_GENERATE, old_level);
+}
+
+static void _pregen_levels(const branch_type branch)
+{
+    unwind_bool no_more(crawl_state.show_more_prompt, false);
+
+    for (int i = 1; i <= branches[branch].numlevels; i++)
+    {
+        level_id new_level = level_id(branch, i);
+        level_pos pos = level_pos(new_level);
+        dprf("Pregenerating %s:%d", branches[pos.id.branch].abbrevname,
+                                    pos.id.depth);
+        _ensure_level_generated(pos);
+    }
+}
+
+static void _pregen_dungeon()
+{
+    mpr("Press <space> to pregenerate (most of) the dungeon."
+            " This will take some time.");
+    more();
+
+    const level_id &pregen_old_level = level_id::current();
+    const coord_def &pregen_old_pos = you.pos();
+    level_pos old_level_pos = level_pos(pregen_old_level, pregen_old_pos);
+    dprf("Before pregen; branch: %d depth: %d Coord: %d, %d.",
+            old_level_pos.id.branch, old_level_pos.id.depth,
+            old_level_pos.pos.x, old_level_pos.pos.y);
+
+    // bel's original proposal generated D to lair depth, then lair, then D
+    // to orc depth, then orc, then the rest of D. I have simplified this to
+    // just generate whole branches at a time -- I am not sure how much real
+    // impact this has. One idea might be to shuffle this slightly based on
+    // the seed.
+    // TODO: probably need to do portal vaults too?
+    // Should this use something like logical_branch_order?
+    const vector<branch_type> generation_order =
+    {
+        BRANCH_DUNGEON,
+        BRANCH_TEMPLE,
+        BRANCH_LAIR,
+        BRANCH_ORC,
+        BRANCH_SPIDER,
+        BRANCH_SNAKE,
+        BRANCH_SHOALS,
+        BRANCH_SWAMP,
+        BRANCH_VAULTS,
+        BRANCH_CRYPT,
+        BRANCH_DEPTHS,
+        BRANCH_VESTIBULE,
+        BRANCH_ELF,
+        BRANCH_ZOT,
+        BRANCH_SLIME,
+        BRANCH_TOMB,
+        BRANCH_TARTARUS,
+        BRANCH_COCYTUS,
+        BRANCH_DIS,
+        BRANCH_GEHENNA,
+    };
+
+    // TODO: why is dungeon invalid? it's not set up properly in
+    // `initialise_branch_depths` for some reason.
+    for (auto br : generation_order)
+        if (brentry[br].is_valid() || br == BRANCH_DUNGEON)
+            _pregen_levels(br);
+}
+#endif
+
 static void _post_init(bool newc)
 {
     ASSERT(strwidth(you.your_name) <= MAX_NAME_LENGTH);
@@ -258,15 +368,25 @@ static void _post_init(bool newc)
 
     run_map_local_preludes();
 
-    // Abyssal Knights start out in the Abyss.
-	if (newc && (you.char_class == JOB_ABYSSAL_KNIGHT))
-		you.where_are_you = BRANCH_ABYSS;
-	else if (newc && you.char_class == JOB_PRIEST)
-		you.where_are_you = BRANCH_START_TEMPLE;
-	else if (newc && you.char_class == JOB_NOBLE)
-		you.where_are_you = BRANCH_START_MARKET;
-    else if (newc)
-        you.where_are_you = root_branch;
+    if (newc)
+    {
+#ifndef DGAMELAUNCH
+        if (Options.pregen_dungeon)
+            _pregen_dungeon();
+#endif
+        you.entering_level = false;
+        you.transit_stair = DNGN_UNSEEN;
+        you.depth = 1;
+		// Abyssal Knights start out in the Abyss.
+		if (you.char_class == JOB_ABYSSAL_KNIGHT)
+			you.where_are_you = BRANCH_ABYSS;
+		else if (you.char_class == JOB_PRIEST)
+			you.where_are_you = BRANCH_START_TEMPLE;
+		else if (you.char_class == JOB_NOBLE)
+			you.where_are_you = BRANCH_START_MARKET;
+		else
+			you.where_are_you = root_branch;
+    }
 
     // XXX: Any invalid level_id should do.
     level_id old_level;