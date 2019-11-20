crawl/crawl

Clean up and improve gozag gold detection
This consolidates a bunch of gozag code and removes an ASSERT that I
think was over-eager, firing when update_item_at doesn't lead to a gold
pile in the stash tracker. There had previously been two versions of
this code, and only one had the check, but it doesn't seem so obvious to
me that update_item_at should really be expected to make any arbitrary
gold pile visible in the stash.

This commit also makes gold detection work in the abyss beyond the
initial levelgen.

crawl-ref/source/god-passive.h
@@ -281,6 +281,7 @@ int ash_skill_boost(skill_type sk, int scale);
 bool ash_has_skill_boost(skill_type sk);
 map<skill_type, int8_t> ash_get_boosted_skills(eq_type type);
 int gozag_gold_in_los(actor* whom);
+void gozag_detect_level_gold(bool count);
 int qazlal_sh_boost(int piety = you.piety);
 int tso_sh_boost();
 void qazlal_storm_clouds();

Bcadren/BcadrenCrawl

crawl-ref/source/god-passive.h
@@ -283,6 +283,7 @@ int ash_skill_boost(skill_type sk, int scale);
 bool ash_has_skill_boost(skill_type sk);
 map<skill_type, int8_t> ash_get_boosted_skills(eq_type type);
 int gozag_gold_in_los(actor* whom);
+void ash_gozag_level_scan(bool count);
 int qazlal_sh_boost(int piety = you.piety);
 int tso_sh_boost();
 void qazlal_storm_clouds();