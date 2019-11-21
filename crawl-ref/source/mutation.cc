Hellmonk/bloatcrawl2

Make ds, wn, and dswn starts more deterministic by seed
This should make the rng for setting up each of these start categories
fully determined by the game seed, and independent from each other (as
well as all other start choices). Before, these were somewhat
accidentally dependent on seed, but dswn led to an interaction between
the two (and potentially with any other randomness that might happen
during character initialization, if there is any). Even if we decide
that this is a bad idea at some point, these are still the right code
changes to make these rolls independent of the rng as well -- would just
need to initialize the subgenerator differently.

Thanks to discord user Blue for pointing out the dswn interaction.

crawl-ref/source/mutation.cc
@@ -2557,6 +2557,12 @@ _schedule_ds_mutations(vector<mutation_type> muts)
 
 void roll_demonspawn_mutations()
 {
+    // intentionally create the subgenerator either way, so that this has the
+    // same impact on the current main rng for all chars.
+    rng::subgenerator ds_rng;
+
+    if (you.species != SP_DEMONSPAWN)
+        return;
     you.demonic_traits = _schedule_ds_mutations(
                          _order_ds_mutations(
                          _select_ds_mutations()));

Bcadren/BcadrenCrawl

crawl-ref/source/mutation.cc
@@ -2585,6 +2585,12 @@ _schedule_ds_mutations(vector<mutation_type> muts)
 
 void roll_demonspawn_mutations()
 {
+    // intentionally create the subgenerator either way, so that this has the
+    // same impact on the current main rng for all chars.
+    rng::subgenerator ds_rng;
+
+    if (you.char_class != JOB_DEMONSPAWN)
+        return;
     you.demonic_traits = _schedule_ds_mutations(
                          _order_ds_mutations(
                          _select_ds_mutations()));